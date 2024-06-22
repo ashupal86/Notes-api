aaca39d2b9fd09eccce3a5a3c2d4b40618744eea
ashu <mr.ashu1810@gmail.com>
2024-06-23 01:12:16 +0530

    Production server

--- app.py
+++ app.py
@@ -105,5 +105,5 @@ def login(username:str,password:str):
         return jsonify({"msg":e})   
 
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=9999)
+    app.run(debug=False,host='0.0.0.0',port=9999)
 
