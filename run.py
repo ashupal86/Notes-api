d63e8f68b4fee57a0eac7872b81125e55df0f4a9
ashupal86 <mr.ashu1810@gmail.com>
2024-12-24 17:52:14 +0000

    test2

--- run.py
+++ run.py
@@ -14,22 +14,17 @@ def install_requirements():
     """Install dependencies from requirements.txt."""
     if os.path.exists('requirements.txt'):
         print("Installing dependencies...")
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+        subprocess.check_call(['python3', '-m', 'pip', 'install', '--upgrade', 'pip'])
+        subprocess.check_call(['python3', '-m', 'pip', 'install','--no-cache-dir', '-r', 'requirements.txt'])
         print("Dependencies installed.")
     else:
         print("requirements.txt file not found. Please create it with the necessary dependencies.")
 
-def run_flask_app():
-    """Run the Flask app."""
-    print("Running Flask app...")
-    subprocess.check_call([sys.executable, 'app.py'])
 
 def main():
     """Main function to create virtual environment, install dependencies, and run the Flask app."""
     create_virtualenv()
     install_requirements()
-    run_flask_app()
 
 if __name__ == "__main__":
     main()
